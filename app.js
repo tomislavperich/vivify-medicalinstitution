0d86a55bf050d768dfcc87a586915de491afcf36
Tomislav Peric <toca91@gmail.com>
2018-12-06 10:05:09 +0100

    #LACV-15 Added event logging support

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 
+// Person class
 class Person {
     constructor(fname, lname, appointments = []) {
         this.firstName = fname;
@@ -7,11 +8,13 @@ class Person {
     }
 }
 
+// Doctor class
 class Doctor extends Person {
     constructor(fname, lname, specialty, patients = [], appointments) {
         super(fname, lname, appointments);
         this.specialty = specialty;
         this.patients = patients;
+        logEvent(getTimeDateStr(), `Kreiran doktor ${this.firstName}`);
     }
 
     addPatient(patient) {
@@ -25,12 +28,14 @@ class Doctor extends Person {
     }
 }
 
+// Patient class
 class Patient extends Person {
     constructor(fname, lname, jmbg, caseNum, doctor = null, appointments) {
         super(fname, lname, appointments);
         this.jmbg = jmbg;
         this.caseNumber = caseNum;
         this.chosenDoctor = doctor;
+        logEvent(getTimeDateStr(), `Kreiran pacijent ${this.firstName}`);
     }
 
     setDoctor(doctor) {
@@ -42,15 +47,25 @@ class Patient extends Person {
     }
 }
 
+// Appointment class
 class Appointment {
     constructor(type, date, time) {
         this.type = type;
         this.date = date;
         this.time = time;
+        logEvent(getTimeDateStr(), 'Zakazan pregled');
     }
 }
 
 // Date functions
+function getTimeStr() {
+    let now = new Date(),
+        hh = now.getHours(),
+        mm = now.getMinutes();
+
+    return `${hh}:${mm}`;
+}
+
 function getDateStr() {
     let today = new Date(),
         dd = today.getDate(),
@@ -60,16 +75,49 @@ function getDateStr() {
     return `${dd}.${mm}.${yyyy}`;
 }
 
-function getTimeStr() {
-    let now = new Date(),
-        hh = now.getHours(),
-        mm = now.getMinutes();
-
-    return `${hh}:${mm}`;
+function getTimeDateStr() {
+    let time = getTimeStr(),
+        date = getDateStr();
+    
+    return `${date} ${time}`;
 }
 
-const doctor1 = new Doctor('Milan', 'Jovanovic', 'kardiolog');
-console.log(doctor1);
+function logEvent(dateTime, action) {
+    // Log template:
+    // [DD.MM.YYYY HH:mm] Action
+    console.log(`[${dateTime}] ${action}`);
+}
 
-const patient1 = new Patient('Dragan', 'Ivanovic', 3096997764805, 35667, doctor1);
+// Kreirati pacijenta i doktora
+const doctor1 = new Doctor('Milan', 'Jovanovic', 'kardiolog');
+const patient1 = new Patient('Dragan', 'Ivanovic', 3096997764805, 35667);
+
+// Pacijent Dragan bira doktora Milana
+patient1.doctor = doctor1;
+
+/* Doktor Milan zakazuje pregled za merenje nivoa šećera
+u krvi za pacijenta Dragan */
+doctor1.setAppointment(
+    getDateStr(),
+    getTimeStr(),
+    'BSExamination',
+    patient1
+);
+
+/* Doktor Milan zakazuje pregled za merenje krvnog pritiska 
+za pacijenta Dragan */
+doctor1.setAppointment(
+    getDateStr(),
+    getTimeStr(),
+    'BPExamination',
+    patient1
+)
+
+/* Pacijent Dragan obavlja laboratorijski pregled za merenje 
+nivoa šećera u krvi */
+
+/* Pacijent Dragan obavlja laboratorijski pregled za merenje 
+krvnog pritiska */
+
+console.log(doctor1);
 console.log(patient1);
